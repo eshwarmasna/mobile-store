629a67ddd0b155be18e9dc5d821a7dbf0fc42ee7
eshwarmasnd <masna.eshwar2003@gmail.com>
2025-03-15 12:48:17 +0530

    Mobile Store

--- pages/signin-signup/signin-signup.js
+++ pages/signin-signup/signin-signup.js
@@ -1,76 +1,79 @@
-
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
+        console.log("User is logged in", user);
     } else {
+        console.log("No user logged in");
     }
 });
 
 let db = _db;
 
-function register(theForm) {
-    var inputUsername = document.querySelector("#username").value;
+function register() {
+    var inputUsername = document.querySelector("#username").value.trim();
     var inputPassword = document.querySelector("#password").value;
     var confirmPassword = document.querySelector("#cnf-password").value;
 
-    if (inputPassword == confirmPassword) {
-        firebase.auth().createUserWithEmailAndPassword(inputUsername, inputPassword)
-            .then(e => {
-                var cube = document.querySelector('.mydiv');
-                cube.classList.toggle('styler');
-                document.querySelector('.login input[type=email]').value = "";
-                document.querySelector('.login input[type=password]').value = "";
-                document.querySelector('#cnf-password').value = "";
-                localStorage.setItem('user', JSON.stringify(e.user))
-                db.child(`users/${e.user.uid}`)
-                    .set({
-                        email: inputUsername,
-                        uid: e.user.uid
-                    })
-            })
-            .catch(function (error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                alert(errorMessage, errorCode);
-            });
-    } else {
-        var x = document.querySelector("#cnf-password").value = '';
-        x.placeholder = 'Password Mismatched'
-        x.style.placeholder.color = 'red';
+    if (!inputUsername || !inputPassword || !confirmPassword) {
+        alert("All fields are required.");
+        return false;
     }
 
+    if (inputPassword !== confirmPassword) {
+        document.querySelector("#cnf-password").value = '';
+        document.querySelector("#cnf-password").placeholder = 'Password Mismatched';
+        document.querySelector("#cnf-password").style.color = 'red';
+        return false;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(inputUsername, inputPassword)
+        .then((e) => {
+            let cube = document.querySelector('.mydiv');
+            cube.classList.toggle('styler');
+
+            document.querySelector(".login input[type=email]").value = "";
+            document.querySelector(".login input[type=password]").value = "";
+            document.querySelector("#cnf-password").value = "";
+            
+            localStorage.setItem('user', JSON.stringify(e.user));
+
+            db.child(`users/${e.user.uid}`).set({
+                email: inputUsername,
+                uid: e.user.uid
+            }).catch(error => console.error("Database error:", error));
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+    return false;
 }
 
-
-
-function login(theForm) {
-    var inputUsername = document.querySelector("#username1").value;
+function login() {
+    var inputUsername = document.querySelector("#username1").value.trim();
     var inputPassword = document.querySelector("#password1").value;
-    firebase.auth().signInWithEmailAndPassword(inputUsername, inputPassword)
-        .then(e => {
-            _db.child(`users/${e.user.uid}`).once('value', (snap) => {
 
-                // if user data is present in the database then route to home page else notify the user and delete him.
+    if (!inputUsername || !inputPassword) {
+        alert("All fields are required.");
+        return false;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(inputUsername, inputPassword)
+        .then((e) => {
+            db.child(`users/${e.user.uid}`).once('value', (snap) => {
                 if (snap.val()) {
-                    localStorage.setItem('user', JSON.stringify(e.user))
+                    localStorage.setItem('user', JSON.stringify(e.user));
                     location.replace('./../../index.html');
                 } else {
                     const user = firebase.auth().currentUser;
-                    user.delete().then(function () {
-                        // User deleted.
-                    }).catch(function (error) {
-                        // An error happened.
-                        alert(error)
+                    user.delete().then(() => {
+                        alert('Your account has been deleted by the admin. Please register again.');
+                    }).catch((error) => {
+                        alert(error.message);
                     });
-                    alert('Your accout has been deleted by the admin, Register with a new Account')
                 }
-            })
-
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert(errorMessage, errorCode);
+            });
+        })
+        .catch((error) => {
+            alert(error.message);
         });
-
-
     return false;
 }
